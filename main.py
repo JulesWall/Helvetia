63bd39b7c1fe17398edd55714d5e34e2891bf2b0
JulesWall <63977848+JulesWall@users.noreply.github.com>
2020-08-17 18:25:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    if is_maintenance:
-        
+    print('We have logged in as {0.user}'.format(client))      
     
 @client.event
 async def on_message(message):
