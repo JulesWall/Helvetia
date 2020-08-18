368585b3a0810988adce59ca68a9b55549bd9dce
JulesWall <63977848+JulesWall@users.noreply.github.com>
2020-08-18 19:59:39 +0200

    Adding converting system (part 1)

--- commands.py
+++ commands.py
@@ -1,9 +1,12 @@
 from command.misc.ping import *
 from command.game.profil import *
+from command.game.bank import *
 
 commands = {
     "ping":[Ping, False],
     "profil":[Profil, True],
     "p":[Profil, True],
-    "pro":[Profil, True]
+    "pro":[Profil, True],
+    "bank":[Bank, True]
 }
+
