70cd6a13b370b5dad50b8c2a74a067c8bc20984f
JulesWall <63977848+JulesWall@users.noreply.github.com>
2020-08-18 18:00:20 +0200

    fix some sintaxs errors

--- db/Registration.py
+++ db/Registration.py
@@ -11,12 +11,12 @@ class NewPlayer:
         self.user_name = user_name
         self.creation_date = date
         self.btc = 0
-        self.dollar = 0
+        self.dollars = 0
 
         db = MySQLdb.connect(user=USERDATABASE, passwd=PASSWORDDATABASE, host="localhost", db=DATABASE)#connexion
         cursor = db.cursor() #création du cursor
-        sql = f"INSERT INTO `player`(`user_id`, `user_name`, `creation_date``btc`, `dollar`)\
-        VALUES ({self.user_id},'{self.user_name}','{self.creation_date},{self.btc},{self.dollar}')"
+        sql = f"INSERT INTO `player`(`user_id`, `user_name`, `creation_date`, `btc`, `dollars`)\
+        VALUES ({self.user_id},'{self.user_name}',{self.creation_date},{self.btc},{self.dollars})"
         cursor.execute(sql) #fonction sql
         db.commit()
         db.close()
