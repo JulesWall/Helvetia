f1fec628944bab8380becc7022f5aa2cb1daa0a8
unknown <mineclashdu34@gmail.com>
2021-01-15 18:43:35 +0100

    ping.py was translated in js and renammed to ping.js

---
+++ command/misc/ping.js
@@ -0,0 +1,10 @@
+
+const Discord = require("discord.js"); // Voir index.js
+
+exports.run = async (message, client, args, lang) => { // Remplace def __init__ / class Ping()
+        var now = Date.now(); // Permet d'avoir la date en ms (timestamp), équivalent du time.monotonic()
+        message.channel.send(lang.Ping).then((msg) => { // Envoies le message et le garde pour plus tard
+            var after = Date.now() - now; // Calcul le ping
+            msg.edit(`${lang.Ping} ${after} ms`); // Edit le message
+        })
+}
