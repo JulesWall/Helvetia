9b9e41e5a3906a68a14ec7e13e08d4df3a54645d
unknown <mineclashdu34@gmail.com>
2021-01-16 22:51:40 +0100

    Function to add Emoji added!

---
+++ function/addEmoji.js
@@ -0,0 +1,8 @@
+module.exports = function(emoji) {
+    if (emoji.toLowerCase() === "wood") return "<:wood:800116555366662186>";
+    if (emoji.toLowerCase() === "stone") return "<:stone:800114756534534144>";
+    if (emoji.toLowerCase() === "iron") return "<:iron:800116516917608461>";
+    if (emoji.toLowerCase() === "gold") return "<:gold:800116537872744449>";
+    if (emoji.toLowerCase() === "level") return "<:level:800112565698428938>";
+    if (emoji.toLowerCase() === "exp") return "<:Exp:800111577075941427>";
+}
