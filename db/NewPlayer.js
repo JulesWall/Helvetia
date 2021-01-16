907df8ecb5c667864b6b8998a2a4b9f93365ab61
unknown <mineclashdu34@gmail.com>
2021-01-16 22:49:30 +0100

    NewPlayer send now a complete and comprehensible message

--- db/NewPlayer.js
+++ db/NewPlayer.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports = function (message, client) {
-    message.channel.send('Oui oui ça marche');
-    require("./Registration.js").NewPlayer(message.author.id, message.author.username, null, "male", "Mine", "OB")
+    message.channel.send(`Yeah! You are now register! (You can change bot language with \`!lang\`)`);
+    require("./Registration.js").NewPlayer(message)
 }
