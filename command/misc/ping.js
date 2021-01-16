935a8c0687b4de8cbc0b24e1eb98415825704476
unknown <mineclashdu34@gmail.com>
2021-01-16 22:57:08 +0100

    ping.js command is available

--- command/misc/ping.js
+++ command/misc/ping.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js"); // Voir index.js
 
 exports.run = async (message, client, args, lang) => { // Remplace def __init__ / class Ping()
         var now = Date.now(); // Permet d'avoir la date en ms (timestamp), équivalent du time.monotonic()
-        message.channel.send(lang.Ping).then((msg) => { // Envoies le message et le garde pour plus tard
+        message.channel.send(lang.ping.latency).then((msg) => { // Envoies le message et le garde pour plus tard
             var after = Date.now() - now; // Calcul le ping
-            msg.edit(`${lang.Ping} ${after} ms`); // Edit le message
+            msg.edit(`${lang.ping.latency} ${after} ms`); // Edit le message
         })
 }
